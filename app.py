bc9b6cd1aaa16537b7852b269c2b9ac3de059cc9
Cody <hotcococody@gmail.com>
2025-12-16 00:22:35 -0800

    Create DB tables on startup

--- app.py
+++ app.py
@@ -265,8 +265,9 @@ def create_app():
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run()
 
